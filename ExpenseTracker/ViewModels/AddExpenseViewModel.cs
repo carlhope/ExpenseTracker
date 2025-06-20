5211b3c3f0d127597eee9a134a764060b02452a8
Carl hope <84421131+carlhope@users.noreply.github.com>
2025-06-20 14:28:59 +0100

    Create AddExpense ViewModel

---
+++ ExpenseTracker/ViewModels/AddExpenseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpenseTracker.Models;
+using System.ComponentModel;
+using System.Windows.Input;
+
+
+namespace ExpenseTracker.ViewModels
+{
+    class AddExpenseViewModel : INotifyPropertyChanged
+
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
