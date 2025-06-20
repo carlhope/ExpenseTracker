d7082f312d55bc07b5f4c63a2a2772607e3cd53c
Carl hope <84421131+carlhope@users.noreply.github.com>
2025-06-20 15:52:01 +0100

    Add AddExpenseViewModel

--- ExpenseTracker/AddExpenseWindow.xaml.cs
+++ ExpenseTracker/AddExpenseWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using ExpenseTracker.Models;
+using ExpenseTracker.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,34 +21,20 @@ namespace ExpenseTracker
     /// </summary>
     public partial class AddExpenseWindow : Window
     {
+        private AddExpenseViewModel ViewModel => (AddExpenseViewModel)DataContext;
+
         public AddExpenseWindow()
         {
             InitializeComponent();
-            DatePicker.SelectedDate = DateTime.Now;
-            CategoryComboBox.ItemsSource = Enum.GetValues(typeof(ExpenseCategory));
-                
-
+            DataContext = new AddExpenseViewModel();
         }
-        private void SaveExpense_Click(object sender, RoutedEventArgs e)
-        {
-            if (decimal.TryParse(AmountTextBox.Text, out decimal amount))
-            {
-                var expense = new Expense
-                {
-                    Description = DescriptionTextBox.Text,
-                    Category = (ExpenseCategory)CategoryComboBox.SelectedItem,
-                    Amount = amount,
-                    Date = DatePicker.SelectedDate ?? DateTime.Now
-                };
 
-                ExpenseStore.Expenses.Add(expense);
-                MessageBox.Show("Expense added!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
-                this.Close();
-            }
-            else
-            {
-                MessageBox.Show("Please enter a valid amount.", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
-            }
+        private void SaveExpense_Click(object sender, RoutedEventArgs e)
+        { 
+            ViewModel.SaveExpense();
+
+            MessageBox.Show("Expense saved!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            this.Close();
         }
     }
 }
--- ExpenseTracker/ViewModels/AddExpenseViewModel.cs
+++ ExpenseTracker/ViewModels/AddExpenseViewModel.cs
@@ -1,18 +1,81 @@
-﻿using System;
+﻿using ExpenseTracker.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ExpenseTracker.Models;
-using System.ComponentModel;
 using System.Windows.Input;
 
 
 namespace ExpenseTracker.ViewModels
 {
-    class AddExpenseViewModel : INotifyPropertyChanged
-
+    public class AddExpenseViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
+        public ObservableCollection<ExpenseCategory> Categories { get; } = new ObservableCollection<ExpenseCategory>((ExpenseCategory[])Enum.GetValues(typeof(ExpenseCategory)));
+
+        private string _description;
+        public string Description
+        {
+            get => _description;
+            set { _description = value; OnPropertyChanged(nameof(Description)); }
+        }
+
+        private ExpenseCategory _selectedCategory;
+        public ExpenseCategory SelectedCategory
+        {
+            get => _selectedCategory;
+            set
+            {
+                _selectedCategory = value;
+                OnPropertyChanged(nameof(SelectedCategory));
+            }
+        }
+
+        private decimal _amount;
+        public decimal Amount
+        {
+            get => _amount;
+            set
+            {
+                _amount = value;
+                OnPropertyChanged(nameof(Amount));
+            }
+        }
+
+        private DateTime _date = DateTime.Now;
+        public DateTime Date
+        {
+            get => _date;
+            set
+            {
+                _date = value;
+                OnPropertyChanged(nameof(Date));
+            }
+        }
+
+
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        public void SaveExpense()
+        {
+            if (string.IsNullOrWhiteSpace(Description) || Amount <= 0)
+            {
+                // Optional: You could expose a validation message property here
+                return;
+            }
+
+            var expense = new Expense
+            {
+                Description = Description.Trim(),
+                Category = SelectedCategory,
+                Amount = Amount,
+                Date = Date
+            };
+
+            ExpenseStore.Expenses.Add(expense);
+        }
     }
 }
